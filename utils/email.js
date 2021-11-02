134a91d786871036d207ec9d985188bd1f62fd7c
rajckamariya <r.c.kamariya@gmail.com>
2021-11-02 15:04:40 +0530

    Second Commit

--- utils/email.js
+++ utils/email.js
@@ -1,15 +1,37 @@
 const nodemailer = require("nodemailer");
 
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(" ")[0];
+    this.url = url;
+    this.from = `Raj Kamariya <${process.env.EMAIL_FROM}>`;
+  }
+
+  createTransport() {
+    if (process.env.NODE_ENV === "production") {
+      return 1;
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  send(template,subject){
+
+  }
+
+  sendWelcom(){
+    this.send('welcome',)
+  }
+};
+
 const sendEmail = async (options) => {
-  //1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
   //Activate in gmail "less secure app" option
 
   //2) Define the email options
@@ -24,4 +46,3 @@ const sendEmail = async (options) => {
   await transporter.sendMail(mailOptions);
 };
 
-module.exports = sendEmail;
